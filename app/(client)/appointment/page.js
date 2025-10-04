6a5b83c07c9f4bde049368885f0cee151c1bff42
inverse9 <permanawahyu97@gmail.com>
2025-10-04 15:03:51 +0800

    Add Appointment and Layout components with initial styling

---
+++ app/(client)/appointment/page.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Appointment = () => {
+  return (
+    <div className="bg-red-300 h-dvh p-20 px-60">
+      <div className="flex bg-blue-100 p-4">
+        <div className="border bg-yellow-50 w-full flex">
+          <div className="w-20 bg-green-200 flex flex-col items-center justify-center py-4">
+            <div className="font-bold">16</div>
+            <div className="font-bold">Oct</div>
+            <div className="">Senin</div>
+          </div>
+          <div className="m-3"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Appointment;
