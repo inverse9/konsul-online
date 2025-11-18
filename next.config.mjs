c3962e5ef51dd71e65050f7051b15525acc9f65e
inverse9 <permanawahyu97@gmail.com>
2025-11-18 15:30:09 +0800

    feat: Add redirect from root to login page for improved navigation

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: true, // Use true (308 redirect) if this is permanent
+      },
+    ];
+  },
+};
 
 export default nextConfig;
